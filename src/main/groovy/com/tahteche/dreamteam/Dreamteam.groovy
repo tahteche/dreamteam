3eae62eb043317a48b1ad393860d7aac0ca1c7ee
Teche Tah <tahteche@gmail.com>
2021-05-12 00:47:47 -0500

    Fixes to JSON deserialization and printing users

--- src/main/groovy/com/tahteche/dreamteam/Dreamteam.groovy
+++ src/main/groovy/com/tahteche/dreamteam/Dreamteam.groovy
@@ -1,12 +1,19 @@
 package com.tahteche.dreamteam
 
+import com.tahteche.dreamteam.responses.Player
 import com.tahteche.dreamteam.services.PlayersService
 
 class Dreamteam {
     static void main(String[] args) {
         int numberOfPlayers = Integer.parseInt(args[0])
         PlayersService playersService = PlayersService.instance
-        def players = playersService.getPlayers(numberOfPlayers)
-        println players
+        List<Player> players = playersService.getPlayers(numberOfPlayers)
+
+        for(Player player in players) {
+            println player.id
+            println player.name
+            println player.email
+            println " "
+        }
     }
 }
--- src/main/groovy/com/tahteche/dreamteam/services/PlayersService.groovy
+++ src/main/groovy/com/tahteche/dreamteam/services/PlayersService.groovy
@@ -1,6 +1,7 @@
 package com.tahteche.dreamteam.services
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.core.type.TypeReference
 import org.apache.hc.client5.http.fluent.Request
 import com.tahteche.dreamteam.responses.Player
 
@@ -10,7 +11,7 @@ class PlayersService {
     ObjectMapper objectMapper = new ObjectMapper()
 
     List<Player> getPlayers(int numberOfPlayers = 10) {
-        List<Player> players = objectMapper.readValue(Request.get(this.url).execute().returnContent().toString(), List<Player>)
+        List<Player> players = objectMapper.readValue(Request.get(this.url).execute().returnContent().toString(), new TypeReference<List<Player>>() {})
         return players.subList(0, numberOfPlayers)
     }
 }
